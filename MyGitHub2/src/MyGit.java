f9d433ff6edf354a13ec518ac85102520eb90466
nick_zhang <zhangjin001007@hotmail.com>
2015-08-11 16:55:24 +0800

    Add a new line

--- MyGitHub2/src/MyGit.java
+++ MyGitHub2/src/MyGit.java
@@ -4,6 +4,7 @@ public class MyGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("My Git Hub");
+		System.out.println("My private Git Hub");
 	}
 
 }
